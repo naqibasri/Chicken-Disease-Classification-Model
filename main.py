8fe1af84fe2c18514700db6b21fcc0dae9166660
naqib <naqib.asri.294@gmail.com>
2023-06-20 18:51:00 +0800

    prepare base model completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from ChickenDiseaseClassificationModel import logger
 from ChickenDiseaseClassificationModel.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from ChickenDiseaseClassificationModel.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -9,6 +10,19 @@ try:
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
 
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Prepare base model"
+
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
     raise e
