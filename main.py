9ecf3045c94a8e3df10d8b2969574504c25eeb1d
naqib <naqib.asri.294@gmail.com>
2023-06-22 13:19:31 +0800

    dvc added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from ChickenDiseaseClassificationModel import logger
 from ChickenDiseaseClassificationModel.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from ChickenDiseaseClassificationModel.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from ChickenDiseaseClassificationModel.pipeline.stage_03_training import ModelTrainingPipeline
+from ChickenDiseaseClassificationModel.pipeline.stage_04_evaluation import EvaluationPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -31,13 +32,24 @@ except Exception as e:
 STAGE_NAME = "Training"
 
 try: 
-   logger.info(f"*******************")
-   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-   model_trainer = ModelTrainingPipeline()
-   model_trainer.main()
-   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_trainer = ModelTrainingPipeline()
+    model_trainer.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-        logger.exception(e)
-        raise e
+    logger.exception(e)
+    raise e
 
 
+STAGE_NAME = "Evaluation stage"
+
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_evaluation = EvaluationPipeline()
+    model_evaluation.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
